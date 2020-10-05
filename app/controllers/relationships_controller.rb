4d3e54a7063bf780403d291925ea72ecc4020547
EC2 Default User <ec2-user@ip-172-31-79-187.ec2.internal>
2020-10-05 06:49:12 +0000

    [Update] users_index

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,25 +1,31 @@
 class RelationshipsController < ApplicationController
 
   def create
+    path = Rails.application.routes.recognize_path(request.referer)
     @user = User.find(params[:follow_id])
     following = current_user.follow(@user)
     if following.save
-      flash[:success] = "success"
-      redirect_to user_path(@user)
+      if path[:action] == "show"
+        redirect_to user_path(@user)
+      else
+        redirect_to users_path
+      end
     else
-      flash.now[:alert] = "failed"
       redirect_to user_path(@user)
     end
   end
 
   def destroy
+    path = Rails.application.routes.recognize_path(request.referer)
     @user = User.find(params[:follow_id])
     following = current_user.unfollow(@user)
     if following.destroy
-      flash[:success] = "success"
-      redirect_to user_path(@user)
+      if path[:action] == "show"
+        redirect_to user_path(@user)
+      else
+        redirect_to users_path
+      end
     else
-      flash.now[:alert] = "failed"
       redirect_to user_path(@user)
     end
   end
