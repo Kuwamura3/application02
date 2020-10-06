aaff2d8d1ef286f1da59c927658ee641b84fd09c
EC2 Default User <ec2-user@ip-172-31-79-187.ec2.internal>
2020-10-06 08:03:40 +0000

    [Add] search_how

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,36 @@
 class SearchController < ApplicationController
 
   def search
+    @word = params[:search_content]
     if params[:search_content]
       if params[:search_model] == "1"
-        @contents = User.where("name LIKE ?", "%#{params[:search_content]}%")
-        render :search_users
+        if params[:search_how] == "1"
+          @contents = User.where("name LIKE ?", "#{params[:search_content]}")
+          render :search_users
+        elsif params[:search_how] == "2"
+          @contents = User.where("name LIKE ?", "#{params[:search_content]}%")
+          render :search_users
+        elsif params[:search_how] == "3"
+          @contents = User.where("name LIKE ?", "%#{params[:search_content]}")
+          render :search_users
+        else
+          @contents = User.where("name LIKE ?", "%#{params[:search_content]}%")
+          render :search_users
+        end
       else
-        @contents = Book.where("title LIKE ?", "%#{params[:search_content]}%")
-        render :search_books
+        if params[:search_how] == "1"
+          @contents = Book.where("title LIKE ?", "#{params[:search_content]}")
+          render :search_books
+        elsif params[:search_how] == "2"
+          @contents = Book.where("title LIKE ?", "#{params[:search_content]}%")
+          render :search_books
+        elsif params[:search_how] == "3"
+          @contents = Book.where("title LIKE ?", "%#{params[:search_content]}")
+          render :search_books
+        else
+          @contents = Book.where("title LIKE ?", "%#{params[:search_content]}%")
+          render :search_books
+        end
       end
     else
       @contents = User.none
