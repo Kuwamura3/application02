172def2cfeb1452c44ca3dca60c2262319b733b3
EC2 Default User <ec2-user@ip-172-31-79-187.ec2.internal>
2020-10-08 06:46:04 +0000

    [Update] comments_list

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,18 @@
 class CommentsController < ApplicationController
 
   def create
+    # ↓undefined each method for nil class対策
+    @comments = Comment.all
     @book = Book.find(params[:book_id])
     @comment = current_user.comments.new(comment_params)
     @comment.book_id = @book.id
     @comment.save
-    redirect_to book_path(@book)
   end
 
   def destroy
+    @comments = Comment.all
+    @book = Book.find(params[:book_id])
     Comment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
